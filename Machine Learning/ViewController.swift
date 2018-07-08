4c18d3964c2bf865cc97f5b2871a0227035a0285
Andrey Kolybelnikov <blumenfrisch@gmail.com>
2018-07-08 17:39:30 +0200

    Initial Commit

---
+++ Machine Learning/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Machine Learning
+//
+//  Created by Andrey Kolybelnikov on 08/07/2018.
+//  Copyright © 2018 Andrey Kolybelnikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
